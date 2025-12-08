06b999c39c902984da98e93ea1f57508c7b543e2
Julien BOLLINI <julien.bollini@gmail.com>
2025-12-08 21:43:13 +0100

    Bouton Démarrer

--- app.py
+++ app.py
@@ -1,50 +1,46 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import joblib
-import time
-import plotly.graph_objects as go
-import plotly.express as px
-import tensorflow as tf  # Indispensable pour l'Autoencoder
+import time # Nécessaire pour simuler le temps de chargement
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
 
-# --- 1. CONFIGURATION DE LA PAGE ---
-st.set_page_config(
-    page_title="Monitor the reactor",
-    page_icon="🏭",
-    layout="wide"
-)
-
-# --- 2. CSS PERSONNALISÉ ---
+# --- CSS PERSONNALISÉ ---
 st.markdown("""
     <style>
-    .main { background-color: #0E1117; }
-    .stMetric { background-color: #262730; padding: 10px; border-radius: 5px; }
-    div.block-container { padding-top: 2rem; }
+    .main { background-color: #0E1117; }    # Dark Mode
+    #.stMetric { background-color: #262730; padding: 10px; border-radius: 5px; } Chiffres en rouge
     </style>
     """, unsafe_allow_html=True)
+
+# --- MENU ---
+st.sidebar.title("Navigation")
+st.sidebar.page_link("app.py", label="Accueil", icon="🏠")
+st.sidebar.page_link("pages/page_1.py", label="Secours", icon="1️⃣")
+
+# --- INTERFACE PRINCIPALE ---
+st.title("🏭 Monitor the reactor")
+st.markdown("Architecture : Détection d'anomalie + Diagnostic")
+
+st.button("Démarrer", type="primary")
+
+
+
 
 # --- 3. CHARGEMENT DES RESSOURCES (Call API) ---
 
 
-
 # --- 4. SIDEBAR ---
-st.sidebar.image("https://cdn.brandfetch.io/iduL2MR6Zu/w/820/h/216/theme/dark/logo.png?c=1bxid64Mup7aczewSAYMX&t=1764620774839", width=100)
-st.sidebar.title("🎛️ Contrôle")
-st.sidebar.markdown("---")
+#st.sidebar.image("https://cdn.brandfetch.io/iduL2MR6Zu/w/820/h/216/theme/dark/logo.png?c=1bxid64Mup7aczewSAYMX&t=1764620774839", width=100)
+#st.sidebar.title("🎛️ Contrôle")
+#st.sidebar.markdown("---")
 
 
-st.sidebar.markdown("---")
-start_btn = st.sidebar.button("▶️ LANCER", type="primary")
-stop_btn = st.sidebar.button("⏹️ ARRÊTER")
+#st.sidebar.markdown("---")
+#start_btn = st.sidebar.button("▶️ LANCER", type="primary")
+#stop_btn = st.sidebar.button("⏹️ ARRÊTER")
 
-# --- 5. INTERFACE PRINCIPALE ---
-st.title("🏭 Monitor the reactor")
-st.markdown("Architecture : Détection d'anomalie + Diagnostic")
 
 
 # --- 6. BOUCLE DE SIMULATION ---
 
 
-        # --- D. VISUALISATION ---
-
-time.sleep()
+# --- D. VISUALISATION ---
